50761bc8317e19c73ef2a9fc8eb8b12000cfd7b7
inanygt <inanygt@hotmail.com>
2023-10-17 13:58:56 +0200

    setup recipees project

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php include 'header.php' ?>
+
+<div class="recipes-grid">
+   <div class="recipe-card">
+      <h1 class="recipe-title">Carbonara</h1>
+      <div class="recipe-body">
+         <div class="recipe-ingredients">
+            <ul>
+               <li>Pasta</li>
+               <li>Parmigiano</li>
+               <li>Eggs</li>
+               <li>Pepper</li>
+            </ul>
+         </div>
+      </div>
+   </div>
+     <div class="recipe-card">
+      <h1 class="recipe-title">Carbonara</h1>
+      <div class="recipe-body">
+         <div class="recipe-ingredients">
+            <ul>
+               <li>Pasta</li>
+               <li>Parmigiano</li>
+               <li>Eggs</li>
+               <li>Pepper</li>
+            </ul>
+         </div>
+      </div>
+   </div>
+     <div class="recipe-card">
+      <h1 class="recipe-title">Carbonara</h1>
+      <div class="recipe-body">
+         <div class="recipe-ingredients">
+            <ul>
+               <li>Pasta</li>
+               <li>Parmigiano</li>
+               <li>Eggs</li>
+               <li>Pepper</li>
+            </ul>
+         </div>
+      </div>
+   </div>
+     <div class="recipe-card">
+      <h1 class="recipe-title">Carbonara</h1>
+      <div class="recipe-body">
+         <div class="recipe-ingredients">
+            <ul>
+               <li>Pasta</li>
+               <li>Parmigiano</li>
+               <li>Eggs</li>
+               <li>Pepper</li>
+            </ul>
+         </div>
+      </div>
+   </div>
+     <div class="recipe-card">
+      <h1 class="recipe-title">Carbonara</h1>
+      <div class="recipe-body">
+         <div class="recipe-ingredients">
+            <ul>
+               <li>Pasta</li>
+               <li>Parmigiano</li>
+               <li>Eggs</li>
+               <li>Pepper</li>
+            </ul>
+         </div>
+      </div>
+   </div>
+     <div class="recipe-card">
+      <h1 class="recipe-title">Carbonara</h1>
+      <div class="recipe-body">
+         <div class="recipe-ingredients">
+            <ul>
+               <li>Pasta</li>
+               <li>Parmigiano</li>
+               <li>Eggs</li>
+               <li>Pepper</li>
+            </ul>
+         </div>
+      </div>
+   </div>
+</div>
+
+<?php include 'footer.php' ?> 
